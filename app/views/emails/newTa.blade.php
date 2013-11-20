aff8011f99e5cfafa964c154055c89b93280a47b
Andrey Kulakevich <andrey@cs.dal.ca>
2013-11-19 23:34:15 -0400

    Fixed minor mistake

--- app/views/emails/newTa.blade.php
+++ app/views/emails/newTa.blade.php
@@ -4,7 +4,7 @@
 		<br>
 		<p>You have been added as a Teaching Assistant to the Learning Centre.</p>
 		<p>Please setup your account by going to the following URL:</p>
-		<p>{{ link_to('ta/setup?email='.$email, "TA setup" }}</p>
+		<p>{{ link_to('ta/setup?email='.$email, "TA setup") }}</p>
 		<br>
 		<p>Thanks,</p>
 		<p>Learning Centre Administration</p>
