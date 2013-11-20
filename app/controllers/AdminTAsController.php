069d0264ee133da66a91a2116e15fb4775a36a14
Andrey Kulakevich <andrey@cs.dal.ca>
2013-11-19 22:30:21 -0400

    Added TAs page for admin. Included Controller, Route and View.

---
+++ app/controllers/AdminTAsController.php
@@ -0,0 +1,59 @@
+<?php
+
+class AdminTAsController extends AdminController {
+
+
+	public function getTas()
+	{
+		$this->navbar['TAs']['active'] = true;
+
+		$tas = TA::all();
+
+		// echo "<pre>";
+		// foreach ($tas as $key => $ta) {
+		// 	echo $ta->user()->email;
+		// }
+		// echo "</pre>";
+		// exit();
+
+		return View::make('admin/TAs')
+					->with('tas',$tas)
+					->with('user', $this->user)
+					->with('navbar', $this->navbar);
+	}
+
+	public function postTa()
+	{
+		Mail::send('emails.newTa', array(), function($message)
+		{
+			$email = Input::get('email');
+
+			$message->to($email)->subject('Welcome to the Learning Centre!');
+		});
+	}
+
+	public function deleteTA($id)
+	{
+		TA::destroy($id);
+
+		return Redirect::to('admin/tas');
+	}
+
+	public function postTeachingAssistants()
+	{
+		// $settings = Input::all();
+
+		// unset($settings['_token']);
+
+		// foreach ($settings as $key => $value) {
+		// 	$setting = Settings::get($key);
+		// 	$setting->value = $value;
+		// 	$setting->save();
+		// }
+		
+		// Session::flash('success',true);
+
+		// return Redirect::to('admin/availability');
+	}
+
+}
