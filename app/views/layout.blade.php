a9869088ccd0f8deb9a8dcef83fc1fefdd2521f9
Andrey Kulakevich <andrey@cs.dal.ca>
2013-11-27 00:55:01 -0400

    Added nicer display for browsing files. Added deactivate button.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -14,11 +14,16 @@
             type="text/css" 
             rel="stylesheet"
             href="{{ asset('css/bootstrap-select.min.css') }}" />
+        <link
+            type="text/css" 
+            rel="stylesheet"
+            href="{{ asset('css/bootstrap-input-file.min.css') }}" />
         <script type="text/javascript" src="{{ asset('js/jquery-1.10.2.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/jquery-ui-1.10.3.custom.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/underscore-min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/bootstrap-select.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/bootstrap-input-file.min.js') }}"></script>
         <title>
             Learning Centre Application
         </title>
--- app/views/ta/profile.blade.php
+++ app/views/ta/profile.blade.php
@@ -3,13 +3,20 @@
     <div class="col-md-12">
         <fieldset class="col-md-12">
         	<legend>My Information</legend>
-        	{{ Form::open(array('url' => 'ta/profile', 'role' =>"form", 'files' => true)) }}
+        	{{ Form::open(array('url' => 'ta/profile', 'method'=>'put', 'role' =>"form", 'files' => true)) }}
 		        <div class="row">
-		        	<div class="col-md-3">
+		        	<div class="col-md-4">
 		        		<div class="form-group">
 							{{ Form::label('picture', 'Image') }} <br>
-								<img src="{{ asset('images/'.$ta->picture) }}" alt="{{ $ta->name }}" class="img-thumbnail">
-							{{ Form::file('picture', array("class" => "form-control") ) }}
+								<img src="{{ asset('images/'.$ta->picture) }}" width="100%" alt="{{ $ta->name }}" class="img-thumbnail">
+							<div class="input-group">
+								<span class="input-group-btn">
+									<span class="btn btn-success btn-file">
+										Browse… <input type="file" id="picture" class="browse-file" name="picture">
+									</span>
+								</span>
+								<input type="text" class="form-control file-text" readonly="">
+							</div>
 							@if ( $error = $errors->first("picture") )
 								<div class="alert alert-warning alert-dismissable">
 					            	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
@@ -47,11 +54,14 @@
 		        	</div>
 		        </div>
 		        
-		        <div class="row">
-		        	{{ Form::submit("Save Changes", array('class'=>'btn btn-primary btn-lg center-block') ) }}
-		        </div>
-			{{ Form::close() }}
+		        <div class="col-md-4 col-md-offset-4">
+		        	{{ Form::submit("Save Changes", array('class'=>'btn btn-primary btn-lg btn-block') ) }}
+		        	{{ Form::close() }}
 
+					{{ Form::open(array('url' => 'ta/profile', 'role' => 'form', 'method'=>'delete', 'class'=>'inline')) }}
+						{{ Form::submit("Deactivate", array('class'=>'btn btn-danger btn-lg btn-block') ) }}
+					{{ Form::close() }}
+		        </div>
         </fieldset>
     </div>
 @stop
