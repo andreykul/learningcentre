f65dece7942484af30be235ca008f669797d7776
Andrey Kulakevich <andrey@cs.dal.ca>
2013-12-04 21:28:14 -0400

    Modified Seeder to display better test cases

--- app/database/seeds/ShiftsTableSeeder.php
+++ app/database/seeds/ShiftsTableSeeder.php
@@ -4,12 +4,12 @@ class ShiftsTableSeeder extends Seeder {
 
     public function run()
     {
-        DB::table('settings')->delete();
+        DB::table('shifts')->delete();
 
         $ta = User::where('role','=','ta')->first()->TA();
 
-        //Create shift every week for 14 weeks starting today
-        for ($i=0; $i < 14; $i++) { 
+        //Create shifts this week and next week
+        for ($i=0; $i < 2; $i++) { 
             $time = time() + ($i * 7 * 24 * 60 * 60);
 
             Shift::create(
@@ -21,5 +21,41 @@ class ShiftsTableSeeder extends Seeder {
                 )
             );
         }
+
+        //Shift at the begging of the Week, Sunday
+        $time = time() - date('w') * 24 * 60 * 60;
+
+        Shift::create(
+            array(
+            'ta_id' => $ta->id,
+            'date' => date("Y-m-d", $time),
+            'start' => 1000,
+            'end' => 1250
+            )
+        );
+
+        //Shift at the end of the Week
+        $time += 6 * 24 * 60 * 60;
+
+        Shift::create(
+            array(
+            'ta_id' => $ta->id,
+            'date' => date("Y-m-d", $time),
+            'start' => 1500,
+            'end' => 1650
+            )
+        );
+
+        //Shift at the begging of the next Week
+        $time += 1 * 24 * 60 * 60;
+
+        Shift::create(
+            array(
+            'ta_id' => $ta->id,
+            'date' => date("Y-m-d", $time),
+            'start' => 1500,
+            'end' => 1650
+            )
+        );
     }
 }
