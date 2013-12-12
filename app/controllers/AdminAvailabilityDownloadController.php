d6a88450efa953b2003d810542dd11e98bde29a0
Andrey Kulakevich <andrey@cs.dal.ca>
2013-12-12 16:29:01 -0400

    Modified Availability Download controller to be able to handle download all

--- app/controllers/AdminAvailabilityDownloadController.php
+++ app/controllers/AdminAvailabilityDownloadController.php
@@ -3,10 +3,58 @@
 class AdminAvailabilityDownloadController extends AdminController {
 
 	public function postIndex(){
-		$day = Input::get('day');
-		$availabilities = Availability::day($day);
 
+		header('Content-Description: File Transfer');
+
+		$day = Input::get('day');
+
+		if ($day == "All")
+		{
+			header('Content-Type: application/zip');
+
+			$days = array(
+        			"Sunday",
+        			"Monday",
+        			"Tuesday",
+        			"Wednesday",
+        			"Thursday",
+        			"Friday",
+        			"Saturday"
+        		);
+
+			$zipname = date("F_Y")."_".$day;
+			$zip = new ZipArchive;
+			$zip->open($zipname, ZipArchive::CREATE);
+			foreach ($days as $day) {
+				$zip->addFromString(date("F_Y")."_".$day.'.csv', $this->generateCSV($day));
+			}
+			$zip->close();
+
+			header('Content-disposition: attachment; filename='.$zipname.'.zip');
+			header('Content-Length: ' . filesize($zipname));
+			readfile($zipname);
+		}
+		else
+		{
+			header('Content-type: text/plain');
+			header('Content-Disposition: attachment; filename='.date("F_Y")."_".$day.".csv");
+			echo $this->generateCSV($day);
+		}
+	}
+
+
+	//Method to generate CSV for a certain day
+	private function generateCSV($day)
+	{
 		$data = array();
+
+		$tas = TA::active();
+
+		foreach ($tas as $ta) {
+			$data[$ta->id] = array();
+		}
+
+		$availabilities = Availability::day($day);
 
 		foreach ($availabilities as $availability) {
 			for ($i=$availability->start; $i < $availability->end; $i+=50) { 
@@ -33,11 +81,7 @@ class AdminAvailabilityDownloadController extends AdminController {
 			}
 			$csv .= "\n";
 		}
-
-		header('Content-Description: File Transfer');
-    	header('Content-type: text/plain');
-    	header('Content-Disposition: attachment; filename='.date("F_Y")."_".$day.".csv");
 		
-		echo $csv;
+		return $csv;
 	}
 }
