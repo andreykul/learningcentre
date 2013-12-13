601c2951ca91a4c54835ce6efd20c77bda803b17
Andrey Kulakevich <andrey@cs.dal.ca>
2013-12-12 20:01:17 -0400

    Added table for schedule based on issue-23

---
+++ app/database/migrations/2013_12_12_234219_create_schedule_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScheduleTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('schedule', function($table){
+			$table->increments('id');
+			$table->integer('ta_id');
+			$table->integer('start')->unsigned()->default(0);
+			$table->integer('end')->unsigned()->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('schedule');
+	}
+
+}
