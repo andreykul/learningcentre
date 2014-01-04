a423727143c350dcac4e025332503e91887707a9
Andrey Kulakevich <andrey@cs.dal.ca>
2014-01-04 17:22:53 -0400

    Made minor changes to the text of the email

--- app/views/emails/shiftAvailable.blade.php
+++ app/views/emails/shiftAvailable.blade.php
@@ -2,7 +2,7 @@
 	<body>
 		<p>Hello {{ $name }}, </p>
 		<br>
-		<p>There is a shift available from {{ Number::toTime($shift->start) }} to {{ Number::toTime($shift->end) }} on {{ date('l', strtotime($shift->date)) }}, {{ date("F dS, Y",strtotime($shift->date)) }} </p>
+		<p>There is a shift available from {{ Number::toTime($shift->start) }} to {{ Number::toTime($shift->end) }} on {{ date('l', strtotime($shift->date)) }}, {{ date("F jS",strtotime($shift->date)) }}. </p>
 		<br>
 		<p>To cover the shift {{ link_to("ta/shifts?week_start=$week_start", "Click Here") }}</p>
 		<br>
